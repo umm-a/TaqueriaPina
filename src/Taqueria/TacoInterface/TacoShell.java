6e0f408d8776c707ac8a9109f21109fb00b339bd
Emelie Ramenberg <gelehallonet@gmail.com>
2022-12-16 09:39:16 +0100

    Lagt tillbaka String name i basklasserna.

--- src/Taqueria/TacoInterface/TacoShell.java
+++ src/Taqueria/TacoInterface/TacoShell.java
@@ -1,11 +1,11 @@
 package Taqueria.TacoInterface;
 
 public class TacoShell implements Taco {
+    private final String name = "Hårt skal";
     private final double price = 89.00;
     @Override
     public String getDescription() {
-        String description = "Hårt skal";
-        return description + " (" + price + " kr)";
+        return name + " (" + price + " kr)";
     }
     @Override
     public double getPrice() {
